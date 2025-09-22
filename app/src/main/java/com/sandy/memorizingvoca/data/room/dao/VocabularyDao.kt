44a846f5a0388cbce8477672f45d283f2b8fb8db
SANDY <nnal0256@naver.com>
2025-09-22 18:15:32 +0900

    VocabularyDao Room 쿼리함수 작성

--- app/src/main/java/com/sandy/memorizingvoca/data/room/dao/VocabularyDao.kt
+++ app/src/main/java/com/sandy/memorizingvoca/data/room/dao/VocabularyDao.kt
@@ -1,8 +1,52 @@
 package com.sandy.memorizingvoca.data.room.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.sandy.memorizingvoca.data.room.entities.Vocabulary
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface  VocabularyDao {
+interface VocabularyDao {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun addVocabularyList(vocaList: List<Vocabulary>)
+
+    @Update
+    suspend fun updateHighlight(vocabulary: Vocabulary)
+    @Update
+    suspend fun updateBookmark(vocabulary: Vocabulary)
+
+    @Query(
+        "SELECT DISTINCT day " +
+                "FROM voca_list"
+    )
+    suspend fun getAllDays(): List<Int>
+
+    @Query(
+        "SELECT * FROM voca_list " +
+                "WHERE day = :day"
+    )
+    suspend fun getVocaList(day: Int): List<Vocabulary>
+
+    @Query(
+        "SELECT * FROM voca_list " +
+                "WHERE bookmarked = :bookmarked"
+    )
+    fun getBookmarkList(bookmarked: Boolean = true): Flow<List<Vocabulary>>
+
+    @Query(
+        "SELECT * FROM voca_list " +
+                "WHERE highlighted = :highlighted"
+    )
+    fun getHighlightList(highlighted: Boolean = true): Flow<List<Vocabulary>>
+
+    @Query(
+        "SELECT * FROM voca_list " +
+                "WHERE vocaId = :vocaId"
+    )
+    suspend fun getVocabulary(vocaId: Int): Vocabulary
 
 }
