8ee3a785dd612d919c742e1d0015ddaadfb45f64
SANDY <nnal0256@naver.com>
2025-09-22 21:38:47 +0900

    data 처리 의존성주입을 위한 DataModule 구현

---
+++ app/src/main/java/com/sandy/memorizingvoca/data/di/DataModule.kt
@@ -0,0 +1,54 @@
+package com.sandy.memorizingvoca.data.di
+
+import com.sandy.memorizingvoca.data.repository.BookmarkRepository
+import com.sandy.memorizingvoca.data.repository.GetQuizRepository
+import com.sandy.memorizingvoca.data.repository.GetVocabularyRepository
+import com.sandy.memorizingvoca.data.repository.HighlightRepository
+import com.sandy.memorizingvoca.data.repository.QuizRepository
+import com.sandy.memorizingvoca.data.repository.VocabularyRepository
+import com.sandy.memorizingvoca.data.repository.impl.BookmarkRepositoryImpl
+import com.sandy.memorizingvoca.data.repository.impl.GetQuizRepositoryImpl
+import com.sandy.memorizingvoca.data.repository.impl.GetVocabularyRepositoryImpl
+import com.sandy.memorizingvoca.data.repository.impl.HighlightRepositoryImpl
+import com.sandy.memorizingvoca.data.repository.impl.QuizRepositoryImpl
+import com.sandy.memorizingvoca.data.repository.impl.VocabularyRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class DataModule {
+
+    @Binds
+    abstract fun bindsBookmarkRepository(
+        impl: BookmarkRepositoryImpl
+    ): BookmarkRepository
+
+    @Binds
+    abstract fun bindsGetQuizRepository(
+        impl: GetQuizRepositoryImpl
+    ): GetQuizRepository
+
+    @Binds
+    abstract fun bindsGetVocabularyRepository(
+        impl: GetVocabularyRepositoryImpl
+    ): GetVocabularyRepository
+
+    @Binds
+    abstract fun bindsHighlightRepository(
+        impl: HighlightRepositoryImpl
+    ): HighlightRepository
+
+    @Binds
+    abstract fun bindsQuizRepository(
+        impl: QuizRepositoryImpl
+    ): QuizRepository
+
+    @Binds
+    abstract fun bindsVocabularyRepository(
+        impl: VocabularyRepositoryImpl
+    ): VocabularyRepository
+
+}
