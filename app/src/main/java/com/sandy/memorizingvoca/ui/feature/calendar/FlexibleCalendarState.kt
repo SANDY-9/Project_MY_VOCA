4fb1af4b50fb566b541002188ae41bfaa4bef7cf
SANDY <nnal0256@naver.com>
2025-09-28 21:48:35 +0900

    FlexibleCalendar 드래그 동작 버그 수정

--- app/src/main/java/com/sandy/memorizingvoca/ui/feature/calendar/FlexibleCalendarState.kt
+++ app/src/main/java/com/sandy/memorizingvoca/ui/feature/calendar/FlexibleCalendarState.kt
@@ -5,45 +5,49 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.input.pointer.PointerInputChange
 
-internal class FlexibleCalendarState {
+internal class FlexibleCalendarState(
+    private val expandFraction: Float,
+    private val normalFraction: Float,
+    private val smallFraction: Float,
+) {
 
     var type by mutableStateOf(CalendarType.EXPANDED_CALENDAR)
         private set
 
-    var fraction by mutableFloatStateOf(1f)
+    var fraction by mutableFloatStateOf(expandFraction)
         private set
 
     private var scrollState by mutableStateOf(CalendarScroll.NONE)
 
     private fun updateType() {
         type = when {
-            fraction > 0.5f -> CalendarType.EXPANDED_CALENDAR
-            fraction > 0.12f -> CalendarType.NORMAL_CALENDAR
+            fraction > normalFraction -> CalendarType.EXPANDED_CALENDAR
+            fraction > smallFraction -> CalendarType.NORMAL_CALENDAR
             else -> CalendarType.SMALL_CALENDAR
         }
     }
 
     fun onVerticalDrag(change: PointerInputChange, dragAmount: Float) {
-        if(dragAmount > 0 && fraction < 1f) {
+        if(dragAmount > 0 && fraction < expandFraction) {
             fraction += 0.01f
             scrollState = CalendarScroll.DOWN_SCROLL
         }
-        if(dragAmount < 0 && fraction > 0.12f) {
+        if(dragAmount < 0 && fraction > smallFraction) {
             fraction -= 0.01f
             scrollState = CalendarScroll.UP_SCROLL
         }
+        if(fraction > 1f) fraction = expandFraction
         updateType()
     }
 
     fun onDragEnd()  {
         fraction = when {
-            fraction > 0.5f -> if(scrollState == CalendarScroll.UP_SCROLL) 0.5f else 12f
-            fraction > 0.12f -> if(scrollState == CalendarScroll.UP_SCROLL) 0.12f else 0.5f
-            else -> 0.12f
+            fraction > normalFraction -> if(scrollState == CalendarScroll.UP_SCROLL) normalFraction else expandFraction
+            fraction > smallFraction -> if(scrollState == CalendarScroll.UP_SCROLL) smallFraction else normalFraction
+            else -> smallFraction
         }
         scrollState = CalendarScroll.NONE
         updateType()
@@ -52,10 +56,17 @@ internal class FlexibleCalendarState {
 }
 
 @Composable
-internal fun rememberFlexibleCalendarState(): FlexibleCalendarState {
-    val scope = rememberCoroutineScope()
+internal fun rememberFlexibleCalendarState(
+    expandFraction: Float = 1f,
+    normalFraction: Float = 0.5f,
+    smallFraction: Float = 0.1f,
+): FlexibleCalendarState {
     val state = remember {
-        FlexibleCalendarState()
+        FlexibleCalendarState(
+            expandFraction = expandFraction,
+            normalFraction = normalFraction,
+            smallFraction = smallFraction,
+        )
     }
     return state
 }
